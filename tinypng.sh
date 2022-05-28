ec8a81a8070acd417779de3cd4743c9b4e722b1e
zhuweitung <zhuweitung@foxmail.com>
2022-05-28 10:52:48 +0800

    更新 tinypng.sh

--- tinypng.sh
+++ tinypng.sh
@@ -5,16 +5,20 @@ last_add_img_files=$(git log --pretty=format:"" --name-status -1 | grep '^A(jpg\
 if [[ ${#last_add_img_files[@]} > 0 ]]; then
     mkdir tinypng_tmp
     echo 'create tinypng_tmp'
+    add_file_num=0
     for file in $last_add_img_files; do
-        [[ -f $file ]] && cp $file tinypng_tmp && echo -e "move ${file} to tinypng_tmp"
+        if [[ -f $file ]]; then
+            let add_file_num += 1
+            cp $file tinypng_tmp && echo -e "move ${file} to tinypng_tmp"
+        fi
     done
-    cd tinypng_tmp && super-tinypng && cp output/* ../ && echo 'compress success' && cd ..
+    [[ $add_file_num > 0 ]] && cd tinypng_tmp && super-tinypng && cp output/* ../ && echo 'compress success' && cd ..
     rm -rf tinypng_tmp
 fi
 
 last_update_img_files=$(git log --pretty=format:"" --name-status -1 | grep '^M(jpg\|png\|gif\|ico)' | awk '{print $2}')
 if [[ ${#last_update_img_files[@]} > 0 ]]; then
     for file in $last_update_img_files; do
-        curl -s https://cdn.jsdelivr.net/gh/acerbot/picbed/${file} && echo -e "flush jsdelivr cdn of ${file}"
+        [[ -f $file ]] && echo -e "will flush jsdelivr cdn of ${file}" && curl -s https://cdn.jsdelivr.net/gh/acerbot/picbed/${file} && echo 'flush success'
     done
 fi

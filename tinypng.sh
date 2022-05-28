0ce16f72a50370250d7208d22b21c6b37f86fbac
zhuweitung <zhuweitung@foxmail.com>
2022-05-28 11:41:43 +0800

    删除压缩图片的action

--- tinypng.sh
+++ tinypng.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 获取最新提交的新增图片文件
-last_add_img_files=$(git log --pretty=format:"" --name-status -1 | grep '^A(jpg\|png\|gif\|ico)' | awk '{print $2}')
+last_add_img_files=$(git show --pretty="" --name-status HEAD | grep '^A(jpg\|png\|gif\|ico)' | awk '{print $2}')
 if [[ ${#last_add_img_files[@]} > 0 ]]; then
     mkdir tinypng_tmp
     echo 'create tinypng_tmp'
@@ -16,7 +16,7 @@ if [[ ${#last_add_img_files[@]} > 0 ]]; then
     rm -rf tinypng_tmp
 fi
 
-last_update_img_files=$(git log --pretty=format:"" --name-status -1 | grep '^M(jpg\|png\|gif\|ico)' | awk '{print $2}')
+last_update_img_files=$(git show --pretty="" --name-status HEAD | grep '^M(jpg\|png\|gif\|ico)' | awk '{print $2}')
 if [[ ${#last_update_img_files[@]} > 0 ]]; then
     for file in $last_update_img_files; do
         [[ -f $file ]] && echo -e "will flush jsdelivr cdn of ${file}" && curl -s https://cdn.jsdelivr.net/gh/acerbot/picbed/${file} && echo 'flush success'
